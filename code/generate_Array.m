a37aed6ac24fb19a7d683b1fbe5d67c36ae8c1c2
ludics <ludi7319@163.com>
2020-06-05 23:21:44 +0800

    almost done

--- code/generate_Array.m
+++ code/generate_Array.m
@@ -1,8 +1,28 @@
 function H = generate_Array(p, j, k)
-A = circshift(eye(p),[0, 1]);
+A = circshift(eye(p), 1);
+% A = gf(A, 1);
 H = zeros(p * j, p * k);
+% H = gf(H, 1);
+iH = zeros(j, k);
 for ia=1:j
 	for ib=1:k
-		H((ia-1)*p+1:ia*p,(ib-1)*p+1:ib*p) = A ^ ((ia-1) * (ib-1));
+                t = mod(ib + ia - 2, k) + 1;
+		if ia > t
+			iH(ia, mod(ib + ia - 2, k)+ 1) = -1;
+		else
+			iH(ia, mod(ib + ia - 2, k)+ 1) = ((ia-1) * (ib-1));
+		end
 	end
+end
+for ia=1:j
+	for ib=1:k
+		if iH(ia, ib) == -1
+			H((ia-1)*p+1:ia*p,(ib-1)*p+1:ib*p) = 0;
+		else
+			H((ia-1)*p+1:ia*p,(ib-1)*p+1:ib*p) = A ^ iH(ia, ib);
+		end
+		% iH(ia, ib) = ((ia-1) * (ib-1));
+	end
+end
+H = H(:, end:-1:1);
 end
--- code/main.m
+++ code/main.m
@@ -1,5 +1,7 @@
 clear;
 
+%%
+% BJ-LDPC
 EbNomin = 1.1;
 EbNomax = 4.5;
 EbNovec = EbNomin:0.2:EbNomax;
@@ -15,11 +17,14 @@ berfit(EbNovec, bj_ber, fitEbNo, [], 'exp');
 figure(2)
 berfit(EbNovec, bj_block_err_rate-eps, fitEbNo, [], 'exp');
 
+%%
+% PG-LDPC
 EbNomin = 1.1;
 EbNomax = 3.9;
 EbNovec = EbNomin:0.2:EbNomax;
 fitEbNo = EbNomin:0.1:EbNomax;
 siglen = 813;
+% pg_ldpc.m;
 load('GF_points.mat');
 H = generate_PG(GF, points);
 [pg_ber, pg_block_err_rate] = get_BER(H, siglen, EbNomin, EbNomax);
@@ -29,14 +34,20 @@ berfit(EbNovec, pg_ber, fitEbNo, [], 'exp');
 figure(4)
 berfit(EbNovec, pg_block_err_rate-eps, fitEbNo, [], 'exp');
 
+%%
+% Array LDPC
+EbNomin = 1.1;
+EbNomax = 4.3;
+EbNovec = EbNomin:0.2:EbNomax;
+fitEbNo = EbNomin:0.1:EbNomax;
 
-p = 31; k = 31; j = 5;
-siglen = 806;
-H = generate_Array(p, j, k);
+p = 31; k = 33; j = 5;
+siglen = 868;
+H = arrayLDPC(p, j, k);
 [ar_ber, ar_block_err_rate] = get_BER(H, siglen, EbNomin, EbNomax);
-% save('data_ar.mat', 'ar_ber', 'ar_block_err_rate');
-
-% figure(5)
-% berfit(EbNovec, ar_ber, fitEbNo, [], 'exp');
-% figure(6)
-% berfit(EbNovec, ar_block_err_rate, fitEbNo, [], 'exp');
+save('data_ar.mat', 'ar_ber', 'ar_block_err_rate');
+% 
+figure(5)
+berfit(EbNovec, ar_ber, fitEbNo, [], 'exp');
+figure(6)
+berfit(EbNovec, ar_block_err_rate-eps, fitEbNo, [], 'exp');
